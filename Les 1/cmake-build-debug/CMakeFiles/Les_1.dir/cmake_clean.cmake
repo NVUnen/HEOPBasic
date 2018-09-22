7032efb4ee1a4f3df87900b2b767b9fc7fe43b0a
Niels <nielsvanunen@gmail.com>
2018-09-22 15:36:40 +0200

    Les 1

---
+++ Les 1/cmake-build-debug/CMakeFiles/Les_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Les_1.dir/main.cpp.obj"
+  "Les_1.pdb"
+  "Les_1.exe"
+  "Les_1.exe.manifest"
+  "libLes_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Les_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
